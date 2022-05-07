cf7f482ff44cb20c511ca64bdd9272fa4d146718
Claude Stephane M. Kouame <claudestephane2010@live.fr>
2022-05-07 15:51:03 +0200

    config handler updated for windows

--- auxiliary/toml.go
+++ auxiliary/toml.go
@@ -2,13 +2,20 @@ package auxiliary
 
 import (
 	"github.com/laurent22/toml-go"
+	"runtime"
 )
 
 /*
  Get required field from config file.
 */
 func GetFromConfig(configField string) string {
+	os := runtime.GOOS
+	config_path := "/crypto/config/config.toml"
 	var parser toml.Parser
-	config := parser.ParseFile("/crypto/config/config.toml")
+	if os == "windows" {
+		config_path = "C:\\crypto\\config\\config.toml"
+	}
+
+	config := parser.ParseFile(config_path)
 	return config.GetString(configField)
 }
